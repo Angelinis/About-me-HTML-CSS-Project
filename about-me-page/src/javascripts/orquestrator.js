c6197e8c6979dd6e15b76b7c66f697782fa5ac39
Angelo Coronado <angmogollon@gmail.com>
2026-02-16 04:06:25 +0100

    Changed import files

--- about-me-page/src/javascripts/orquestrator.js
+++ about-me-page/src/javascripts/orquestrator.js
@@ -1,5 +1,17 @@
 import { createEntity } from "./createEntity.js";
 
+// Import all assets as modules:
+import vrglasses from "../assets/objects/lowpoly_vr_glasses.glb";
+import navmesh from "../assets/objects/navmesh(3).gltf";
+import museumModel from "../assets/objects/museum2.glb";
+import unityLogo from "../assets/objects/unity_engine_logo.glb";
+import cLogo from "../assets/objects/c_logo.glb";
+import javascriptLogo from "../assets/objects/javascript_logo.glb";
+import mailLogo from "../assets/objects/mail.glb";
+import linkedinLogo from "../assets/objects/linkedin_logo.glb";
+import developerModel from "../assets/objects/developer.glb";
+import musicSound from "../assets/objects/music.wav";
+
 window.addEventListener("DOMContentLoaded", () => {
   const scene = document.querySelector("#root");
 
@@ -13,128 +25,108 @@ window.addEventListener("DOMContentLoaded", () => {
     color: "#ECECEC"
   });
 
-const player = `
+  const player = `
   <a-entity id="rig"
-            movement-controls="constrainToNavMesh: true;
-                               controls: checkpoint, gamepad, trackpad, keyboard, nipple;"
+            movement-controls="constrainToNavMesh: true; controls: checkpoint, gamepad, trackpad, keyboard, nipple;"
             position="1.5 0.3 -3.6">
-    <a-entity camera
-              position="0 1.6 0"
-              look-controls>
-    </a-entity>
+    <a-entity camera position="0 1.6 0" look-controls></a-entity>
   </a-entity>
-`;
+  `;
 
-scene.insertAdjacentHTML('beforeend', player);
+  scene.insertAdjacentHTML('beforeend', player);
 
-const cursor = `
+  const cursor = `
  <a-entity
         id="cursor"
         cursor="rayOrigin: mouse"
         raycaster="objects: .interactable; far: Infinity"
         mouse-manipulation>
   </a-entity>  
-`;
+  `;
 
-scene.insertAdjacentHTML('beforeend', cursor);
+  scene.insertAdjacentHTML('beforeend', cursor);
 
-const vr = createEntity("a-entity", {
-  id: "sound-source",
-   "gltf-model": "./src/assets/objects/lowpoly_vr_glasses.glb",
-     position: "3.2 2.0 -4.9",
-     scale: "2 2 2",
-     animation: "property: position; to: 3.2 2.2 -4.9; dur: 2000; easing: easeInOutSine; loop: true;  dir: alternate",
-sound:"src: url(./src/assets/objects/music.wav); loop: true; distanceModel: linear; refDistance: 1; maxDistance: 4",
-     "start-audio-on-click": "",
-      class: "interactable movable"
-
-    //  animation: "property: rotation; to: 0 360 0; loop: true; dur: 10000"
-}
-)
-
-vr.addEventListener('click', () => {
-  vr.components.sound.playSound();
-});
-
-
-
-const navMesh = createEntity("a-entity", {
-  "gltf-model": "./src/assets/objects/navmesh(3).gltf",
-  "nav-mesh": "",
-     position: "7.0 5.2 1.3",
-     scale: "0.3 0.3 0.25",
-     visible: "false"
+  const vr = createEntity("a-entity", {
+    id: "sound-source",
+    "gltf-model": vrglasses,
+    position: "3.2 2.0 -4.9",
+    scale: "2 2 2",
+    animation: "property: position; to: 3.2 2.2 -4.9; dur: 2000; easing: easeInOutSine; loop: true; dir: alternate",
+    sound: `src: url(${musicSound}); loop: true; distanceModel: linear; refDistance: 1; maxDistance: 4`,
+    "start-audio-on-click": "",
+    class: "interactable movable"
   });
 
+  vr.addEventListener('click', () => {
+    vr.components.sound.playSound();
+  });
+
+  const navMesh = createEntity("a-entity", {
+    "gltf-model": navmesh,
+    "nav-mesh": "",
+    position: "7.0 5.2 1.3",
+    scale: "0.3 0.3 0.25",
+    visible: "false"
+  });
 
   const museum = createEntity("a-entity", {
-    "gltf-model": "./src/assets/objects/museum2.glb",
-     position: "7.0 5.2 1.3",
-     scale: "0.3 0.3 0.25"
+    "gltf-model": museumModel,
+    position: "7.0 5.2 1.3",
+    scale: "0.3 0.3 0.25"
   });
 
   const unity_logo = createEntity("a-entity", {
-    "gltf-model": "./src/assets/objects/unity_engine_logo.glb",
-     position: "-0.7 2.8 -12.0",
-     scale: "0.1 0.1 0.1"
+    "gltf-model": unityLogo,
+    position: "-0.7 2.8 -12.0",
+    scale: "0.1 0.1 0.1"
   });
 
-    const c_logo = createEntity("a-entity", {
-    "gltf-model": "./src/assets/objects/c_logo.glb",
-     position: "-2.9 1.2 -12.0",
-     scale: "0.02 0.02 0.02"
+  const c_logo = createEntity("a-entity", {
+    "gltf-model": cLogo,
+    position: "-2.9 1.2 -12.0",
+    scale: "0.02 0.02 0.02"
   });
 
-      const javascript_logo = createEntity("a-entity", {
-    "gltf-model": "./src/assets/objects/javascript_logo.glb",
-     position: "-1 0.8 -12.0",
-     scale: "9 9 9"
+  const javascript_logo = createEntity("a-entity", {
+    "gltf-model": javascriptLogo,
+    position: "-1 0.8 -12.0",
+    scale: "9 9 9"
   });
 
-
-javascript_logo.addEventListener('model-loaded', () => {
-  javascript_logo.object3D.traverse((node) => {
-    if (node.isMesh) {
-      node.material.metalness = 0.2; // lower metallic shine
-      node.material.roughness = 0.6; // softer, less sharp highlights
-    }
+  javascript_logo.addEventListener('model-loaded', () => {
+    javascript_logo.object3D.traverse((node) => {
+      if (node.isMesh) {
+        node.material.metalness = 0.2; // lower metallic shine
+        node.material.roughness = 0.6; // softer, less sharp highlights
+      }
+    });
   });
-});
 
+  const mail_logo = createEntity("a-entity", {
+    "gltf-model": mailLogo,
+    position: "14.6 2.1 -11.0",
+    rotation: "0 -60 0",
+    scale: "0.4 0.4 0.4",
+    class: "interactable clickable",
+    clickable: "url: mailto:angelocoronado@discente.ufg.br"
+  });
 
-const mail_logo = createEntity("a-entity", {
-  "gltf-model": "./src/assets/objects/mail.glb",
-  position: "14.6 2.1 -11.0",
-  rotation: "0 -60 0",
-  scale: "0.4 0.4 0.4",
-  class: "interactable clickable",
-  clickable: "url: mailto:angelocoronado@discente.ufg.br" 
-});
-
-
-const linkedin_logo = createEntity("a-entity", {
-  "gltf-model": "./src/assets/objects/linkedin_logo.glb",
-  position: "15.8 2.1 -9.2",
-  rotation: "0 -50 0",
-  scale: "0.6 0.6 0.6",
-  class: "interactable clickable",
-  clickable: "url: https://www.linkedin.com/in/angelo-coronado-m/" 
-});
+  const linkedin_logo = createEntity("a-entity", {
+    "gltf-model": linkedinLogo,
+    position: "15.8 2.1 -9.2",
+    rotation: "0 -50 0",
+    scale: "0.6 0.6 0.6",
+    class: "interactable clickable",
+    clickable: "url: https://www.linkedin.com/in/angelo-coronado-m"
+  });
 
   const developer = createEntity("a-entity", {
-    "gltf-model": "./src/assets/objects/developer.glb",
-     position: "0 0.3 -10.0",
-     scale: "0.22 0.22 0.22",
-     "animation-mixer": "clip: *;"
+    "gltf-model": developerModel,
+    position: "0 0.3 -10.0",
+    scale: "0.22 0.22 0.22",
+    "animation-mixer": "clip: *;"
   });
-
-
-
-
-
 
   // Append everything
   scene.append(environment, sky, navMesh, museum, unity_logo, javascript_logo, c_logo, mail_logo, linkedin_logo, vr, developer);
-
-
 });
